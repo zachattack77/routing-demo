71bb49a01bb071d12fbe20ce474982b5850786de
Zach Kunitsa <zkunitsa@yahoo.com>
2018-01-12 14:49:57 -0500

    added to roots

--- index.php
+++ index.php
@@ -3,8 +3,13 @@
 //require the autoload file
 require_once('vendor/autoload.php');
 
+
+
 //create an istance of the base class
 $f3 = Base::instance();
+
+//set debug level
+$f3->set ('DEBUG', 3);
 
 //define a default route
 $f3->route('GET /', function() {
@@ -14,6 +19,16 @@ $f3->route('GET /', function() {
 
     }
 );
+
+//define a page1 route
+$f3->route('GET /page1', function(){
+    echo '<h1>this is page 1 </h1>';
+});
+
+
+$f3->route('GET /page2', function(){
+    echo '<h1>this is page 2 </h1>';
+});
 
 //run Fat-Free
 $f3->run();
