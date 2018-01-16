2ef5bddd41f764a12d682bde9ad94ed21e75c10e
Zach Kunitsa <zkunitsa@yahoo.com>
2018-01-16 13:27:17 -0500

    changed index.php

--- index.php
+++ index.php
@@ -12,8 +12,10 @@ $f3 = Base::instance();
 $f3->set ('DEBUG', 3);
 
 //define a default route
-$f3->route('GET /', function() {
+$f3->route('GET /jewelry/rings/toe-rings', function() {
     echo '<h1>Hello!</h1>';
+    $template = new Template();
+    echo $template->render('views/toe-rings.html');
     //echo '<h2>How are you?</h2>';
 
 
@@ -21,14 +23,13 @@ $f3->route('GET /', function() {
 );
 
 //define a page1 route
-$f3->route('GET /page1', function(){
-    echo '<h1>this is page 1 </h1>';
-});
+//$f3->route('GET /jewelry/rings/toe-rings', function(){
+   // echo '<h1>this is page 1 </h1>';
+//});
 
 
-$f3->route('GET /page2', function(){
-    echo '<h1>this is page 2 </h1>';
-});
+
+
 
 //run Fat-Free
 $f3->run();
