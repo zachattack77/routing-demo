25718f0441e719f011114f1eb66c2c05a95739c8
Zach Kunitsa <zkunitsa@yahoo.com>
2018-01-16 13:58:07 -0500

    index.php

--- index.php
+++ index.php
@@ -11,6 +11,41 @@ $f3 = Base::instance();
 //set debug level
 $f3->set ('DEBUG', 3);
 
+//define a route usinf parameters
+$f3->route('GET /hello/@name',
+    function($f3, $params){
+    //$name = $params['name'];
+    //echo "<h1>Hello, $name</h1>";
+
+        $f3->set('name', $params['name']);
+    $template = new Template();
+    echo $template->render('views/hello.html');
+
+});
+
+//define a route using parameters
+$f3->route('GET /language/@lang',
+    function($f3, $params){
+        switch ($params['lang']){
+            case 'swahili':
+                echo 'Jumbo!'; break;
+            case 'spanish':
+                echo 'Hola!'; break;
+            case 'russian':
+                echo 'Privet!'; break;
+            case 'farsi':
+                echo 'Jumbo!'; break;
+
+                //reroute to another page
+            case 'french':
+                $f3->reroute('/');
+             //404 error
+            default:
+                $f3->error(404);
+        }
+        //echo "<h1>Hello, $name</h1>";
+    });
+
 //define a default route
 $f3->route('GET /jewelry/rings/toe-rings', function() {
     echo '<h1>Hello!</h1>';
